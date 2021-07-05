686c4a1ef1e62b0dc72b8be1446c8045c7fbf330
BpHana <57785153+BpHana@users.noreply.github.com>
2021-07-05 17:56:53 +0700

    add login-screen.dart

---
+++ lib/screens/login-screen.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(),
+          ),
+          Container(
+            height: double.infinity,
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 120),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: "OpenSans",
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 30),
+                  _buildEmailTF(),
+                  SizedBox(height: 30),
+                  _buildPasswordTF(),
+                  _buildForgotPasswordBtn(),
+                  _buildLoginBtn(),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        '- OR -',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Text(
+                        'Sign in with',
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    width: double.infinity,
+                    child: SignInButton(
+                      Buttons.GoogleDark,
+                      padding: EdgeInsets.all(5.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      elevation: 10,
+                      onPressed: () {print("Login with google");},
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 35),
+                    width: double.infinity,
+                    child: SignInButton(
+                      Buttons.FacebookNew,
+                      padding: EdgeInsets.all(11.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      elevation: 10,
+                      onPressed: () => print("Login with facebook"),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => print('Sign Up Button Pressed'),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Don\'t have an Account? ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Sign Up',
+                            style: TextStyle(
+                              color: Colors.deepPurple,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _buildLoginBtn() {
+    return Container(
+                  padding: EdgeInsets.symmetric(vertical: 25.0),
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(15.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      elevation: 10,
+                    ),
+                    onPressed: () => print('Login Button Pressed'),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 1.5,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'OpenSans',
+                      ),
+                    ),
+                  ),
+                );
+  }
+
+  Container _buildForgotPasswordBtn() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: TextButton(
+        style: TextButton.styleFrom(padding: EdgeInsets.only(right: 0)),
+        onPressed: () => print('Forgot Password Button Pressed'),
+        child: Text(
+          'Forgot Password?',
+        ),
+      ),
+    );
+  }
+
+  Column _buildPasswordTF() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Password"),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60,
+          child: TextField(
+            obscureText: true,
+            keyboardType: TextInputType.visiblePassword,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14),
+              prefixIcon: Icon(
+                Icons.vpn_key,
+                color: Colors.black,
+              ),
+              hintText: "Enter your password",
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Column _buildEmailTF() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Email"),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14),
+              prefixIcon: Icon(
+                Icons.email_outlined,
+                color: Colors.black,
+              ),
+              hintText: "Enter your email",
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
